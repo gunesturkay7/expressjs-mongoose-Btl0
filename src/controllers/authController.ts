0a23b709e634f5224382ed7ef02266bf9f408a6b
gunesturkay7 <rgunesturkay@gmail.com>
2023-12-31 00:12:41 +0300

    singup responsea user eklendi

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -22,7 +22,7 @@ const signUp = async (req: Request, res: Response) => {
       }
     );
 
-    res.status(201).json({ message: "User created successfully", token });
+    res.status(201).json({ message: "User created successfully", user, token });
   } catch (error: any) {
     res.status(500).json({ message: "Error creating user", error: error.message });
   }
