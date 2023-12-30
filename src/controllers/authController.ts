75f374ee202c9c77350bef2357bee2c210969d96
gunesturkay7 <rgunesturkay@gmail.com>
2023-12-30 21:30:48 +0300

    tenant ekleme listeleme eklendi auth eklendi

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,24 +3,24 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
 
-const generateToken = (user: IUser) => {
-  // Destructure the password and rest of the user properties
-  const { password, ...userInfoWithoutPassword } = user.toObject();
-
-  // Sign the token with user information except password
-  return jwt.sign(userInfoWithoutPassword, "asdasdsadasd", {
-    expiresIn: "1h",
-  });
-};
-
 const signUp = async (req: Request, res: Response) => {
   try {
     const { username, email, password } = req.body;
     const user: IUser = await UserModel.create({ username, email, password });
 
-    const token = jwt.sign({ id: user._id, username: user.username }, "asdasdsadasd", {
-      expiresIn: "1h",
-    });
+    // Include additional user details in the token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        userAvatar: user.userAvatar,
+      },
+      "asdasdsadasd",
+      {
+        expiresIn: "1h",
+      }
+    );
 
     res.status(201).json({ message: "User created successfully", token });
   } catch (error: any) {
@@ -30,7 +30,6 @@ const signUp = async (req: Request, res: Response) => {
 
 const signIn = async (req: Request, res: Response) => {
   try {
-    console.log(req);
     const { username, password } = req.body;
     const user: IUser | null = await UserModel.findOne({ username });
 
@@ -44,9 +43,19 @@ const signIn = async (req: Request, res: Response) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    const token = jwt.sign({ id: user._id, username: user.username }, "asdasdsadasd", {
-      expiresIn: "1h",
-    });
+    // Include additional user details in the token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        userAvatar: user.userAvatar,
+      },
+      "asdasdsadasd",
+      {
+        expiresIn: "1h",
+      }
+    );
 
     res.status(200).json({ message: "Authentication successful", token, user });
   } catch (error: any) {
