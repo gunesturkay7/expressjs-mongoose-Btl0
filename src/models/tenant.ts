95789aa483460c0cbe11c9c81b02e60eba0279d3
gunesturkay7 <rgunesturkay@gmail.com>
2024-01-02 01:36:05 +0300

    tenant model enhanced

--- src/models/tenant.ts
+++ src/models/tenant.ts
@@ -1,17 +1,35 @@
 import { Document, Schema, model, Types } from "mongoose";
 
 export interface ITenant extends Document {
-  user: Types.ObjectId; // Kullanıcı referansı
+  user: Types.ObjectId;
   name: string;
   address: string;
-  // Diğer özellikler
+  contactNumber: string;
+  email: string;
+  leaseStartDate: Date;
+  leaseEndDate: Date;
+  rentAmount: Number;
+  securityDeposit: Number;
+  rentDueDate: Date;
+  emergencyContact: string;
+  rentalHistory: string;
+  notes: string;
 }
 
 const TenantSchema: Schema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
   address: { type: String, required: true },
-  // Diğer alanlar
+  contactNumber: { type: String, required: true },
+  email: { type: String, required: true },
+  leaseStartDate: { type: Date, required: true },
+  leaseEndDate: { type: Date, required: true },
+  rentAmount: { type: Number, required: true },
+  securityDeposit: { type: Number, required: true },
+  rentDueDate: { type: Date, required: true },
+  emergencyContact: { type: String, required: false },
+  rentalHistory: { type: String, required: false },
+  notes: { type: String, required: false },
 });
 
 export const Tenant = model<ITenant>("Tenant", TenantSchema);
